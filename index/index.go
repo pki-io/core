eec40bb43b1e06df97ead6351854daf968a5bd45
Fraser Scott <fraser.scott@gmail.com>
2014-12-24 15:58:51 +0000

    bigger scope

---
+++ index/index.go
@@ -0,0 +1,188 @@
+package index
+
+import (
+	"fmt"
+	"pki.io/document"
+)
+
+const IndexDefault string = `{
+    "scope": "pki.io",
+    "version": 1,
+    "type": "index-document",
+    "options": "",
+    "body": {
+        "parent-id": "",
+        "tags": {
+          "ca-forward": {},
+          "ca-reverse": {},
+          "entity-forward": {},
+          "entity-reverse": {}
+        }
+    }
+}`
+
+const IndexSchema string = `{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "IndexDocument",
+  "description": "Index Document",
+  "type": "object",
+  "required": ["scope","version","type","options","body"],
+  "additionalProperties": false,
+  "properties": {
+      "scope": {
+          "description": "Scope of the document",
+          "type": "string"
+      },
+      "version": {
+          "description": "Document schema version",
+          "type": "integer"
+      },
+      "type": {
+          "description": "Type of document",
+          "type": "string"
+      },
+      "options": {
+          "description": "Options data",
+          "type": "string"
+      },
+      "body": {
+          "description": "Body data",
+          "type": "object",
+          "required": ["parent-id", "tags"],
+          "additionalProperties": false,
+          "properties": {
+              "parent-id" : {
+                  "description": "Parent ID",
+                  "type": "string"
+              },
+              "tags": {
+                  "description": "Tags",
+                  "type": "object",
+                  "required": ["ca-forward","ca-reverse","entity-forward","entity-reverse"],
+                  "additionalProperties": false,
+                  "properties": {
+                      "ca-forward": {
+                          "description": "Tags to CAs",
+                          "type": "object"
+                      },
+                      "ca-reverse": {
+                          "description": "CA to tags",
+                          "type": "object"
+                      },
+                      "entity-forward": {
+                          "description": "Tags to entities",
+                          "type": "object"
+                      },
+                      "entity-reverse": {
+                          "description": "Entities to tags",
+                          "type": "object"
+                      }
+                  }
+              }
+          }
+      }
+  }
+}`
+
+type IndexData struct {
+	Scope   string `json:"scope"`
+	Version int    `json:"version"`
+	Type    string `json:"type"`
+	Options string `json:"options"`
+	Body    struct {
+		ParentId string `json:"parent-id"`
+		Tags     struct {
+			CAForward     map[string][]string `json:"ca-forward"`
+			CAReverse     map[string][]string `json:"ca-reverse"`
+			EntityForward map[string][]string `json:"entity-forward"`
+			EntityReverse map[string][]string `json:"entity-reverse"`
+		} `json:"tags"`
+	} `json:"body"`
+}
+
+type Index struct {
+	document.Document
+	Data IndexData
+}
+
+func New(jsonString interface{}) (*Index, error) {
+	index := new(Index)
+	index.Schema = IndexSchema
+	index.Default = IndexDefault
+	if err := index.Load(jsonString); err != nil {
+		return nil, fmt.Errorf("Could not create new Index: %s", err.Error())
+	} else {
+		return index, nil
+	}
+}
+
+func (index *Index) Load(jsonString interface{}) error {
+	data := new(IndexData)
+	if data, err := index.FromJson(jsonString, data); err != nil {
+		return fmt.Errorf("Could not load Index JSON: %s", err.Error())
+	} else {
+		index.Data = *data.(*IndexData)
+		return nil
+	}
+}
+
+func (index *Index) Dump() string {
+	if jsonString, err := index.ToJson(index.Data); err != nil {
+		return ""
+	} else {
+		return jsonString
+	}
+}
+
+func AppendUnique(slice []string, val string) []string {
+	found := false
+	for _, v := range slice {
+		if v == val {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		return slice
+	} else {
+		return append(slice, val)
+	}
+}
+
+func (index *Index) AddCATags(ca string, i interface{}) error {
+	var inTags []string
+	switch t := i.(type) {
+	case string:
+		inTags = []string{i.(string)}
+	case []string:
+		inTags = i.([]string)
+	default:
+		return fmt.Errorf("Could not add CA tags. Wrong data type for tags: %T", t)
+	}
+
+	for _, tag := range inTags {
+		index.Data.Body.Tags.CAForward[tag] = AppendUnique(index.Data.Body.Tags.CAForward[tag], ca)
+		index.Data.Body.Tags.CAReverse[ca] = AppendUnique(index.Data.Body.Tags.CAReverse[ca], tag)
+	}
+
+	return nil
+}
+
+func (index *Index) AddEntityTags(entity string, i interface{}) error {
+	var inTags []string
+	switch t := i.(type) {
+	case string:
+		inTags = []string{i.(string)}
+	case []string:
+		inTags = i.([]string)
+	default:
+		return fmt.Errorf("Could not add Entity tags. Wrong data type for tags: %T", t)
+	}
+	for _, tag := range inTags {
+		index.Data.Body.Tags.EntityForward[tag] = AppendUnique(index.Data.Body.Tags.EntityForward[tag], entity)
+		index.Data.Body.Tags.EntityReverse[entity] = AppendUnique(index.Data.Body.Tags.EntityReverse[entity], tag)
+	}
+
+	return nil
+}
---
+++ index/index_test.go
@@ -0,0 +1,59 @@
+package index
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestIndexNew(t *testing.T) {
+	index, err := New(nil)
+	assert.Nil(t, err)
+	assert.NotNil(t, index)
+	assert.Equal(t, index.Data.Scope, "pki.io")
+}
+
+func TestIndexDump(t *testing.T) {
+	index, _ := New(nil)
+	indexJson := index.Dump()
+	assert.NotEqual(t, len(indexJson), 0)
+}
+
+func TestIndexAddCATagsString(t *testing.T) {
+	index, _ := New(nil)
+	err := index.AddCATags("ca1", "tag1")
+	assert.Nil(t, err)
+}
+
+func TestIndexAddCATagsSlice(t *testing.T) {
+	index, _ := New(nil)
+	inIndex := []string{"tag1", "tag2"}
+	err := index.AddCATags("ca2", inIndex)
+	assert.Nil(t, err)
+}
+
+func TestIndexAddEntityTagsString(t *testing.T) {
+	index, _ := New(nil)
+	err := index.AddEntityTags("entity1", "tag1")
+	assert.Nil(t, err)
+}
+
+func TestIndexAddEntityTagsSlice(t *testing.T) {
+	index, _ := New(nil)
+	inIndex := []string{"tag1", "tag2"}
+	err := index.AddEntityTags("entity2", inIndex)
+	assert.Nil(t, err)
+}
+
+func TestIndexAddTags(t *testing.T) {
+	index, _ := New(nil)
+	inTags1 := []string{"tag1", "tag2"}
+	inTags2 := []string{"tag2", "tag3"}
+	err := index.AddEntityTags("entity1", inTags1)
+	assert.Nil(t, err)
+	err = index.AddEntityTags("entity2", inTags2)
+	assert.Nil(t, err)
+	err = index.AddCATags("ca1", inTags2)
+	assert.Nil(t, err)
+	err = index.AddCATags("ca2", inTags1)
+	assert.Nil(t, err)
+}
