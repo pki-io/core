1388229ca076a4c3136024a6add229300affbb30
Fraser Scott <fraser.scott@gmail.com>
2015-02-15 20:54:01 +0000

    nodes need an index for searching etc

--- index/org_test.go
+++ index/org_test.go
@@ -5,47 +5,47 @@ import (
 	"testing"
 )
 
-func TestIndexNew(t *testing.T) {
-	index, err := New(nil)
+func TestOrgIndexNew(t *testing.T) {
+	index, err := NewOrg(nil)
 	assert.Nil(t, err)
 	assert.NotNil(t, index)
 	assert.Equal(t, index.Data.Scope, "pki.io")
 }
 
-func TestIndexDump(t *testing.T) {
-	index, _ := New(nil)
+func TestOrgIndexDump(t *testing.T) {
+	index, _ := NewOrg(nil)
 	indexJson := index.Dump()
 	assert.NotEqual(t, len(indexJson), 0)
 }
 
-func TestIndexAddCATagsString(t *testing.T) {
-	index, _ := New(nil)
+func TestOrgIndexAddCATagsString(t *testing.T) {
+	index, _ := NewOrg(nil)
 	err := index.AddCATags("ca1", "tag1")
 	assert.Nil(t, err)
 }
 
-func TestIndexAddCATagsSlice(t *testing.T) {
-	index, _ := New(nil)
+func TestOrgIndexAddCATagsSlice(t *testing.T) {
+	index, _ := NewOrg(nil)
 	inIndex := []string{"tag1", "tag2"}
 	err := index.AddCATags("ca2", inIndex)
 	assert.Nil(t, err)
 }
 
-func TestIndexAddEntityTagsString(t *testing.T) {
-	index, _ := New(nil)
+func TestOrgIndexAddEntityTagsString(t *testing.T) {
+	index, _ := NewOrg(nil)
 	err := index.AddEntityTags("entity1", "tag1")
 	assert.Nil(t, err)
 }
 
-func TestIndexAddEntityTagsSlice(t *testing.T) {
-	index, _ := New(nil)
+func TestOrgIndexAddEntityTagsSlice(t *testing.T) {
+	index, _ := NewOrg(nil)
 	inIndex := []string{"tag1", "tag2"}
 	err := index.AddEntityTags("entity2", inIndex)
 	assert.Nil(t, err)
 }
 
-func TestIndexAddTags(t *testing.T) {
-	index, _ := New(nil)
+func TestOrgIndexAddTags(t *testing.T) {
+	index, _ := NewOrg(nil)
 	inTags1 := []string{"tag1", "tag2"}
 	inTags2 := []string{"tag2", "tag3"}
 	err := index.AddEntityTags("entity1", inTags1)
@@ -58,8 +58,8 @@ func TestIndexAddTags(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-func TestAddPairingKey(t *testing.T) {
-	index, _ := New(nil)
+func TestOrgIndexAddPairingKey(t *testing.T) {
+	index, _ := NewOrg(nil)
 	id := "123"
 	key := "abc"
 	tags := []string{"tag1", "tag2"}
