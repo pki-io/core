18ebee69bd8db008e0ac838d989fd61249c189ba
Fraser Scott <fraser.scott@gmail.com>
2015-03-03 03:12:22 +0000

    expiry period and tests

--- x509/ca_test.go
+++ x509/ca_test.go
@@ -22,10 +22,15 @@ func TestX509CADump(t *testing.T) {
 
 func TestX509CAGenerateRoot(t *testing.T) {
 	ca, _ := NewCA(nil)
-	err := ca.GenerateRoot(time.Now(), time.Now().AddDate(5, 5, 5))
+	err := ca.GenerateRoot()
 	assert.Nil(t, err)
 	assert.NotEqual(t, ca.Data.Body.Certificate, "")
 	assert.NotEqual(t, ca.Data.Body.Id, "")
+
+	cert, err := PemDecodeX509Certificate([]byte(ca.Data.Body.Certificate))
+	assert.Nil(t, err)
+	assert.True(t, cert.NotBefore.After(time.Now().AddDate(0, 0, -1)))
+	assert.True(t, cert.NotAfter.Before(time.Now().AddDate(0, 0, 1)))
 }
 
 func TestX509CAGenerateSub(t *testing.T) {
@@ -33,13 +38,18 @@ func TestX509CAGenerateSub(t *testing.T) {
 	rootCA.Data.Body.Name = "RootCA"
 	rootCA.Data.Body.DNScope.Country = "UK"
 	rootCA.Data.Body.DNScope.Organization = "pki.io"
-	rootCA.GenerateRoot(time.Now(), time.Now().AddDate(5, 5, 5))
+	rootCA.GenerateRoot()
 
 	subCA, _ := NewCA(nil)
 	subCA.Data.Body.Name = "DevCA"
 	subCA.Data.Body.DNScope.OrganizationalUnit = "Development"
-	err := subCA.GenerateSub(rootCA, time.Now(), time.Now().AddDate(5, 5, 1))
+	err := subCA.GenerateSub(rootCA)
 
 	assert.Nil(t, err)
 	assert.NotEqual(t, subCA.Data.Body.Certificate, "")
+
+	cert, err := PemDecodeX509Certificate([]byte(subCA.Data.Body.Certificate))
+	assert.Nil(t, err)
+	assert.True(t, cert.NotBefore.After(time.Now().AddDate(0, 0, -1)))
+	assert.True(t, cert.NotAfter.Before(time.Now().AddDate(0, 0, 1)))
 }
